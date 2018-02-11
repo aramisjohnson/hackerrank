800775e10af2f039daed0a3f5e742d9b32a48265
Ty Johnson <ty.johnson@quantum.com>
2018-02-11 13:38:04 -0800

    Create CompareTriplet.go

---
+++ CompareTriplet.go
@@ -0,0 +1,33 @@
+package main
+import "fmt"
+import "strings"
+import "strconv"
+import "os"
+import "bufio"
+
+func main() {
+	buf := bufio.NewReader(os.Stdin)
+
+	line1, _ := buf.ReadString('\n')
+	alice := strings.Split(strings.TrimSpace(line1), " ")
+	line2, _ := buf.ReadString('\n')
+	bob := strings.Split(strings.TrimSpace(line2), " ")
+
+	alicePoints := 0
+	bobPoints := 0
+
+	for i := 0; i < len(alice); i++ {
+		aliceScore, _ := strconv.Atoi(alice[i])
+        bobScore, _ := strconv.Atoi(bob[i])
+
+        if aliceScore == bobScore {
+            continue
+        } else if aliceScore > bobScore {
+			alicePoints += 1
+		} else if bobScore > aliceScore {
+			bobPoints += 1
+		}
+	}
+
+	fmt.Printf("%v %v", alicePoints, bobPoints)
+}
